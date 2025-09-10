298ded6e80d36abd5e3c5f07c30502d9df2bf79f
Shivam Singh <shivam.23scse1010271@galgotiasuniversity.ac.in>
2025-09-10 21:31:40 +0530

    Add login and signup pages with authentication logic

--- SIH RAISE std copy/src/App.jsx
+++ SIH RAISE std copy/src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StdDashboard from "./std-dashboard/StdDashboard";
 import LiveLecturePage from "./std-dashboard/LiveLecturePage";
+import Login from "./Pages/Login";
+import Signup from "./Pages/SignUp";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         {/* Dashboard with all its sections (default) */}
         <Route path="/*" element={<StdDashboard />} />
 

e671c110e3fa8e218eac1523c582047b98f0f688
Shivam Singh <shivam.23scse1010271@galgotiasuniversity.ac.in>
2025-09-11 12:20:03 +0530

    Delete unwanted folders

--- student frontend/src/std-dashboard/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Footer.module.css";
-
-export default function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <p className={styles.text}>© 2025 R.A.I.S.E. All rights reserved.</p>
-    </footer>
-  );
-}
